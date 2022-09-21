59ef6e962750a86d0e4ec1a82c98268261866b5f
Raúl <wychouza@gmail.com>
2022-09-20 21:26:56 -0700

    add mix gleam

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Crow.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      archives: [mix_gleam: "~> 0.6.0"],
+      compilers: [:gleam | Mix.compilers()],
+      aliases: [
+        "deps.get": ["deps.get", "gleam.deps.get"]
+      ],
+      erlc_paths: ["build/dev/erlang/crow/build"],
+      erlc_include_path: "build/dev/erlang/crow/include"
     ]
   end
 
@@ -25,7 +32,10 @@ defmodule Crow.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:scenic, "~> 0.11.0-beta.0"},
-      {:scenic_driver_local, "~> 0.11.0-beta.0"}
+      {:scenic_driver_local, "~> 0.11.0-beta.0"},
+      {:gleam_stdlib, "~> 0.22"},
+      {:gleam_otp, "~> 0.5"},
+      {:gleeunit, "~> 0.6", only: [:dev, :test], runtime: false}
     ]
   end
 end
